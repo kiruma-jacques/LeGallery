c7360592882be05e9d1e58e60abca6c0120a1634
jakesIII <jayruma@yahoo.com>
2019-12-17 16:55:28 +0300

    change in database

--- pictures/models.py
+++ pictures/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Upload(models.Model):
-    title = models.CharField(max_length = 30)
-    description = models.TextField()
-    location = models.ForeignKey('pin', default=0)
-    category = models.ForeignKey('Section', default=0)
-    mboto = models.ImageField(upload_to='upload_storage/', blank=True)
-
-    def __str__(self):
-        return self.title
-
-    def save_upload(self):
-        self.save()
-
-    @classmethod
-    def get_all_upload(cls):
-        return cls.objects.all()
-
-    @classmethod
-    def locate_image(cls, search_term):
-        return cls.objects.filter(location__icontains=search_term)
-
 class pin (models.Model):
     pin_name = models.CharField(max_length = 30, null=True, default=0)
 
@@ -39,3 +18,33 @@ class Section(models.Model):
 
     def save_section(self):
         self.save()
+
+    @classmethod
+    def get_all_sections(cls):
+        return cls.objects.all()
+
+class Upload(models.Model):
+    title = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    location = models.ForeignKey('pin', default=0)
+    category = models.ForeignKey('Section', default=0)
+    mboto = models.ImageField(upload_to='upload_storage/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_upload(self):
+        self.save()
+
+    @classmethod
+    def delete_upload(cls, id):
+        return cls.objects.filter(id = id).delete()
+
+    @classmethod
+    def get_all_upload(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def locate_image(cls, search_term):
+        return cls.objects.filter(category__section_name__icontains=search_term)
--- pictures/views.py
+++ pictures/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from .models import Upload
+from .models import Upload, Section
 # Create your views here.
 
 def index(request):
     picha = Upload.get_all_upload()
-    return render(request, 'index.html', {"picha":picha})
+    category=Section.get_all_sections()
+    return render(request, 'index.html', {"picha":picha, "categorys":category})
 
 def search_result(request):
     if 'specific' in request.GET and request.GET["specific"]:
-        search_term = request.GET.get("specification")
+        search_term = request.GET.get("specific")
         upload_located = Upload.locate_image(search_term)
-        message - f"{search_term}"
+        message = f"{search_term}"
         return render(request, "search.html", {"message":message, "images":upload_located})
     else:
         message = 'You have not specified anything'
         return render(request, 'search.html', {"message":message})
+
+def all_category(request):
+    category=Section.get_all_sections()
+    return render(request, 'footer.html', {"categorys":category})
