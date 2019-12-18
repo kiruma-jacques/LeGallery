fe1af18de7de8296536c75c062f13169cdffd9d8
jakesIII <jayruma@yahoo.com>
2019-12-18 10:26:28 +0300

    adjust mode to production mode

--- gallery/settings.py
+++ gallery/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-from decouple import config,Csv
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -99,6 +99,7 @@ else:
            default=config('DATABASE_URL')
        )
    }
+
 db_from_env=dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
