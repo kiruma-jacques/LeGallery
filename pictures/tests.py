860d047b677901a4827705db14764b494443e71b
jakesIII <jayruma@yahoo.com>
2019-12-18 11:42:16 +0300

    test model pin instance

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 from .models import Upload, pin, Section
 # Create your tests here.
+
+class PinTestClass(TestCase):
+    def setUp(self):
+        self.newpin=pin(pin_name = 'states')
+        self.save_pin()
+
+    def test_pin_instance(self):
+        self.assertTrue(isinstance(self.newpin, pin))
