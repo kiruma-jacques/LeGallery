2fa60f8d39f2e7f6332dde72a576f24d68e1fcfc
jakesIII <jayruma@yahoo.com>
2019-12-18 12:00:28 +0300

    pin instance test passed

--- pictures/tests.py
+++ pictures/tests.py
@@ -5,7 +5,8 @@ from .models import Upload, pin, Section
 class PinTestClass(TestCase):
     def setUp(self):
         self.newpin=pin(pin_name = 'states')
-        self.save_pin()
+
+        self.newpin.save_pin()
 
     def test_pin_instance(self):
         self.assertTrue(isinstance(self.newpin, pin))
